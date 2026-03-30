ec7d080273f48b17b4b08337e39476ef0d76e0e9
dennis gega <dennis.gega@uconn.edu>
2026-03-29 22:20:50 -0400

    Move semantics

--- my_unique_ptr.h
+++ my_unique_ptr.h
@@ -15,6 +15,22 @@ public:
         delete m_ptr;
     }
 
+    // Make class move only
+    MyUniquePtr(const MyUniquePtr&) = delete;
+    MyUniquePtr& operator=(const MyUniquePtr&) = delete;
+
+    MyUniquePtr(MyUniquePtr&& other) noexcept
+        : m_ptr{ other.m_ptr }
+    {
+        other.m_ptr = nullptr;
+    }
+
+    MyUniquePtr& operator=(MyUniquePtr&& other) noexcept
+    {
+        m_ptr = other.m_ptr;
+        other.m_ptr = nullptr;
+    }
+
     T& operator*()  const { return *m_ptr; }
     T* operator->() const { return m_ptr; }
     T* get() const { return m_ptr; }
